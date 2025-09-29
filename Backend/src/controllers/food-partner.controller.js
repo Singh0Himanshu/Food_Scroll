3c6531e263d5e7373e25f820e2f4295d33c0305f
Singh0Himanshu <shimanshu.3689@gmail.com>
2025-09-29 23:45:06 +0530

    feat: enhance getFoodPartnerById to include food items associated with the partner

--- Backend/src/controllers/food-partner.controller.js
+++ Backend/src/controllers/food-partner.controller.js
@@ -1,9 +1,11 @@
 const foodPartnerModel = require('../models/foodpartener.model');
+const foodModel = require('../models/food.model')
 
 async function getFoodPartnerById(req,res){
 
     const foodPartnerid = req.params.id;
     const foodpartner = await foodPartnerModel.findById(foodPartnerid);
+    const foodItemsByFoodPartner = await foodModel.find({foodPartner: foodPartnerid})
 
     if(!foodpartner){
         return res.status(400).json({
@@ -13,7 +15,10 @@ async function getFoodPartnerById(req,res){
 
     res.status(200).json({
         message:"Food Partner found",
-        foodpartner
+        foodpartner :{
+            ...foodpartner.toObject(),
+            foodItems: foodItemsByFoodPartner
+        }
     })
 }
 
